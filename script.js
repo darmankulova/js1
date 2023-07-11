a9596d42ec3bdda38547a0f8869a288e4ff85588
darmankulova <imetoffa1@gmail.com>
2023-07-11 12:39:52 +0600

    hw1 is done

---
+++ script.js
@@ -0,0 +1,23 @@
+// let a = 4
+// let b = 55
+// console.log(a+b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a%b);
+
+
+// let a = 'hello'
+// let b = 'Nurperi'
+// console.log(a+' '+b);
+
+// let a = 5
+// let b = undefined
+// console.log(a+b);
+
+// let a = 5
+// let b = '5'
+// console.log(+b+a);
+
+let a = 2
+let b =
+console.log(a*b);
